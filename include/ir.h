3c2f83cea7a0022744a343ca6cfb423e300d8e62
Halvor Bjørstad <43417915+Riphiphip@users.noreply.github.com>
2021-03-02 18:08:46 +0100

    Fix node_create signature

--- include/ir.h
+++ include/ir.h
@@ -1,8 +1,7 @@
 #ifndef IR_H
 #define IR_H
 
-typedef struct n
-{
+typedef struct n {
     node_index_t type;
     void *data;
     void *entry;
@@ -10,8 +9,9 @@ typedef struct n
     struct n **children;
 } node_t;
 
-void node_init(
-    node_t *n, node_index_t type, void *data, uint64_t n_children, ...);
+void node_init (
+    node_t *n, node_index_t type, void *data, uint64_t n_children, ...
+);
 
 /**
  * Allocate and initialize new node
@@ -21,9 +21,11 @@ void node_init(
  * @param ... Pointers to children
  * @return Pointer to new node
  */
-void node_init(node_t *nd, node_index_t type, void *data, uint64_t n_children, va_list children);
+node_t* node_create (
+    node_index_t type, void *data, uint64_t n_children, ...
+);
 
-void node_print(node_t *root, int nesting);
-void node_finalize(node_t *discard);
-void destroy_subtree(node_t *discard);
+void node_print ( node_t *root, int nesting );
+void node_finalize ( node_t *discard );
+void destroy_subtree ( node_t *discard );
 #endif
